9fa31faa694b463166c28309738f85a0d64afd4d
ken_tunc <tunc.adfuisses.53@gmail.com>
2021-12-04 11:54:53 +0900

    [candle-api] Remove `Propagation.MANDATORY`

--- bittrader-candle-api/src/main/kotlin/org/kentunc/bittrader/candle/api/domain/service/CandleService.kt
+++ bittrader-candle-api/src/main/kotlin/org/kentunc/bittrader/candle/api/domain/service/CandleService.kt
@@ -5,13 +5,12 @@ import org.kentunc.bittrader.common.domain.model.candle.Candle
 import org.kentunc.bittrader.common.domain.model.ticker.Ticker
 import org.kentunc.bittrader.common.domain.model.time.Duration
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Propagation
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class CandleService(private val candleRepository: CandleRepository) {
 
-    @Transactional(propagation = Propagation.MANDATORY)
+    @Transactional
     suspend fun feed(ticker: Ticker, duration: Duration): Void? {
         val newCandle = Candle.of(ticker, duration)
         return when (val matched = candleRepository.find(newCandle.id)) {
