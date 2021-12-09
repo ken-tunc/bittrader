973d84e96abd6a6ab59be12092e1145c0d308ed8
ken_tunc <tunc.adfuisses.53@gmail.com>
2021-12-09 20:17:23 +0900

    polish

--- bittrader-candle-api/src/main/kotlin/org/kentunc/bittrader/candle/api/infrastructure/configuration/H2Configuration.kt
+++ bittrader-candle-api/src/main/kotlin/org/kentunc/bittrader/candle/api/infrastructure/configuration/H2Configuration.kt
@@ -2,9 +2,6 @@ package org.kentunc.bittrader.candle.api.infrastructure.configuration
 
 import io.r2dbc.spi.ConnectionFactory
 import org.h2.tools.Server
-import org.kentunc.bittrader.candle.api.infrastructure.repository.development.EnumDao
-import org.kentunc.bittrader.candle.api.infrastructure.repository.development.EnumInserter
-import org.kentunc.bittrader.candle.api.infrastructure.repository.development.H2DatabaseInitializer
 import org.kentunc.bittrader.candle.api.infrastructure.development.H2ConsoleServer
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
@@ -12,7 +9,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.core.io.Resource
-import org.springframework.data.r2dbc.core.R2dbcEntityTemplate
+import org.springframework.r2dbc.connection.init.CompositeDatabasePopulator
 import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer
 import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator
 
@@ -23,22 +20,16 @@ class H2Configuration {
     @Bean
     fun initializer(
         @Qualifier("connectionFactory") connectionFactory: ConnectionFactory,
-        @Value("classpath:schema-h2.sql") resource: Resource
+        @Value("classpath:schema-h2.sql") schemaResource: Resource,
+        @Value("classpath:data-h2.sql") dataResource: Resource
     ): ConnectionFactoryInitializer {
         return ConnectionFactoryInitializer().apply {
             setConnectionFactory(connectionFactory)
-            setDatabasePopulator(ResourceDatabasePopulator(resource))
+            val schemaPopulator = ResourceDatabasePopulator(schemaResource)
+            val dataPopulator = ResourceDatabasePopulator(dataResource)
+            setDatabasePopulator(CompositeDatabasePopulator(schemaPopulator, dataPopulator))
         }
     }
-
-    @Bean
-    fun enumDao(template: R2dbcEntityTemplate) = EnumDao(template)
-
-    @Bean
-    fun enumInserter(enumDao: EnumDao) = EnumInserter(enumDao)
-
-    @Bean
-    fun databaseInitializer(enumInserter: EnumInserter) = H2DatabaseInitializer(enumInserter)
 
     @Bean
     @Profile("local")
---
+++ bittrader-candle-api/src/main/resources/data-h2.sql
@@ -0,0 +1,6 @@
+insert ignore into product_code values ('BTC_JPY');
+insert ignore into product_code values ('ETH_JPY');
+
+insert ignore into duration values ('MINUTES');
+insert ignore into duration values ('HOURS');
+insert ignore into duration values ('DAYS');
