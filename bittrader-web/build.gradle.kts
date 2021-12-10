945661cde25c77ccb862a929a38559b886cf10d0
ken_tunc <tunc.adfuisses.53@gmail.com>
2021-12-10 12:40:57 +0900

    [web] Add bootstrap.

--- bittrader-web/build.gradle.kts
+++ bittrader-web/build.gradle.kts
@@ -1,8 +1,11 @@
+val bootstrapVersion: String by project
+val apexchartsVersion: String by project
 dependencies {
     implementation(project(":bittrader-common"))
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-    implementation("org.webjars.npm:apexcharts:3.30.0")
+    implementation("org.webjars:bootstrap:$bootstrapVersion")
+    implementation("org.webjars.npm:apexcharts:$apexchartsVersion")
     testImplementation(project(":bittrader-test"))
     testImplementation("org.springframework.boot:spring-boot-starter-web")
 }
