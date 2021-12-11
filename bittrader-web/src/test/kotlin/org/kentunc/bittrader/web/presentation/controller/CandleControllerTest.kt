960d2a8c731915520c0a03209100e18c91bad0e3
ken_tunc <tunc.adfuisses.53@gmail.com>
2021-12-12 01:23:12 +0900

    [web] Add tests.

--- bittrader-web/src/test/kotlin/org/kentunc/bittrader/web/presentation/controller/CandleControllerTest.kt
+++ bittrader-web/src/test/kotlin/org/kentunc/bittrader/web/presentation/controller/CandleControllerTest.kt
@@ -5,6 +5,8 @@ import io.mockk.coEvery
 import io.mockk.coVerify
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EnumSource
 import org.kentunc.bittrader.common.domain.model.market.ProductCode
 import org.kentunc.bittrader.common.domain.model.time.Duration
 import org.kentunc.bittrader.common.test.model.TestCandleList
@@ -19,10 +21,43 @@ internal class CandleControllerTest : AbstractControllerTest() {
     private lateinit var candleService: CandleService
 
     @Test
-    fun testCandlestickChart() {
+    fun testCandlestickChart_default() {
         // setup:
         val productCode = ProductCode.BTC_JPY
-        val duration = Duration.DAYS
+        val duration = Duration.MINUTES
+        val candleList = TestCandleList.create()
+        coEvery { candleService.search(any()) } returns candleList
+
+        // exercise & verify:
+        val result = webTestClient.get()
+            .uri {
+                it.path("/candles/{productCode}")
+                    .build(productCode)
+            }
+            .exchange()
+            .expectStatus().isOk
+            .expectBody()
+            .returnResult()
+        MockMvcWebTestClient.resultActionsFor(result)
+            .andExpect(view().name("candle"))
+            .andExpect(model().attribute("productCodes", ProductCode.values()))
+            .andExpect(model().attribute("durations", Duration.values()))
+            .andExpect(model().attribute("activeDuration", duration))
+            .andExpect(model().attributeExists("candleSticks"))
+
+        coVerify {
+            candleService.search(withArg {
+                assertEquals(productCode, it.productCode)
+                assertEquals(duration, it.duration)
+            })
+        }
+    }
+
+    @ParameterizedTest
+    @EnumSource(Duration::class)
+    fun testCandlestickChart_specified(duration: Duration) {
+        // setup:
+        val productCode = ProductCode.BTC_JPY
         val candleList = TestCandleList.create()
         coEvery { candleService.search(any()) } returns candleList
 
