ebabb9761995ce108ec77165041987cecbe536de
ken_tunc <tunc.adfuisses.53@gmail.com>
2021-12-29 00:24:30 +0900

    [candle-api] Add architecuture rule.

--- bittrader-candle-api/src/test/kotlin/org/kentunc/bittrader/candle/api/test/RuleSetsTest.kt
+++ bittrader-candle-api/src/test/kotlin/org/kentunc/bittrader/candle/api/test/RuleSetsTest.kt
@@ -4,8 +4,10 @@ import com.tngtech.archunit.core.importer.ImportOption
 import com.tngtech.archunit.junit.AnalyzeClasses
 import com.tngtech.archunit.junit.ArchTest
 import com.tngtech.archunit.junit.ArchTests
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.methods
 import org.kentunc.bittrader.candle.api.BittraderCandleApiApplication
 import org.kentunc.bittrader.test.rules.LayerDependencyRulesTest
+import org.springframework.transaction.annotation.Transactional
 
 @AnalyzeClasses(
     packagesOf = [BittraderCandleApiApplication::class],
@@ -15,4 +17,9 @@ internal class RuleSetsTest {
 
     @ArchTest
     private val layerDependencyRules = ArchTests.`in`(LayerDependencyRulesTest::class.java)
+
+    @ArchTest
+    private val transactionalAnnotationRule =
+        methods().that().areDeclaredInClassesThat().haveSimpleNameEndingWith("Interactor")
+            .should().beAnnotatedWith(Transactional::class.java)
 }
